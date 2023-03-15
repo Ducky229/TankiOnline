f5ca899d31fa723c4fb0ec5e723b27fba59ec0fa
DURIGER <117347941+DURIGER@users.noreply.github.com>
2023-03-15 11:18:28 +0300

    Update Player.gd

--- Игрок/Player.gd
+++ Игрок/Player.gd
@@ -3,24 +3,20 @@ extends CharacterBody2D
 @export var speed = 100
 @export var rotation_speed = 2
 
-var rotation_direction = 0
+var rotation_direction
 
 var bullet = preload("res://Игрок/bullet.tscn")
 
 func _process(_delta):
-	Fire()
-func Fire():
 	if Input.is_action_pressed("move_fire"):
 		var bullet_instance = bullet.instantiate()
 		bullet_instance.position = $M.position
 		bullet_instance.rotation = get_angle_to(get_global_mouse_position())
 		get_parent().add_child(bullet_instance)
 
-func get_input():
+func _physics_process(delta):
 	rotation_direction = Input.get_axis("move_left", "move_right")
 	velocity = transform.x * Input.get_axis("move_down", "move_up") * speed
-
-func _physics_process(delta):
-	get_input()
 	rotation += rotation_direction * rotation_speed * delta
+	
 	move_and_slide()
